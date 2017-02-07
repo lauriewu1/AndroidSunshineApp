88a07d8332b65567f3074bcb52807f207398a1e9
Laurie <lauriewu1@gmail.com>
2017-02-06 20:26:11 -0800

    updates few java and xml files in order to load relevant weather icons.

--- app/src/main/java/com/example/android/sunshine/app/ForecastAdapter.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastAdapter.java
@@ -77,14 +77,14 @@ public class ForecastAdapter extends CursorAdapter {
         int viewType = getItemViewType(cursor.getPosition());
         switch(viewType){
             case VIEW_TYPE_TODAY:{
-                //get weather icon and set image
+                //get weather icon and set color image
                 viewHolder.iconView.setImageResource(Utility.getArtResourceForWeatherCondition(
                         cursor.getInt(ForecastFragment.COL_WEATHER_CONDITION_ID)));
                 break;
             }
 
             case VIEW_TYPE_FUTURE_DAY:{
-                //get weather icon and set image
+                //get weather icon and set grey image
                 viewHolder.iconView.setImageResource(Utility.getIconResourceForWeatherCondition(
                         cursor.getInt(ForecastFragment.COL_WEATHER_CONDITION_ID)));
                 break;
