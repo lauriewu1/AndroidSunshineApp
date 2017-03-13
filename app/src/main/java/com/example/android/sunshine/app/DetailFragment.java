ec60cafa56fccfc735f2beaed75b20071017c4a9
Laurie <lauriewu1@gmail.com>
2017-03-13 03:14:27 -0700

    updates few java and xml files in order to create 2 pane UI for tablet and maintain same functions for handheld size devices

--- app/src/main/java/com/example/android/sunshine/app/DetailFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/DetailFragment.java
@@ -146,7 +146,7 @@ public class DetailFragment extends Fragment implements
     public Loader<Cursor> onCreateLoader(int id, Bundle args) {
         Log.v(LOG_TAG, "In onCreateLoader");
         Intent intent = getActivity().getIntent();
-        if (intent == null) {
+        if (intent == null || intent.getData() == null) {
             return null;
         }
 
