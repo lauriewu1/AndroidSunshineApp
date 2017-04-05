1323d709d2a1962dd36cebf8238ce9447d4fc178
Laurie <lauriewu1@gmail.com>
2017-04-05 14:43:20 -0700

    Added code to show the status of input location in setting.

--- app/src/main/java/com/example/android/sunshine/app/SettingsActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/SettingsActivity.java
@@ -71,22 +71,43 @@ public class SettingsActivity extends PreferenceActivity
     }
 
     private void setPreferenceSummary(Preference preference, Object value) {
-        String stringValue = value.toString();
         String key = preference.getKey();
+        String stringValue = value.toString();
 
         if (preference instanceof ListPreference) {
-            // For list preferences, look up the correct display value in
-            // the preference's 'entries' list (since they have separate labels/values).
-            ListPreference listPreference = (ListPreference) preference;
-            int prefIndex = listPreference.findIndexOfValue(stringValue);
+            //For list preferences, look up the correct display value in the preference's 'entries'
+            // list (since they have separate labels/values)
+
+            ListPreference listPref = (ListPreference) preference;
+            int prefIndex = listPref.findIndexOfValue(stringValue);
+
             if (prefIndex >= 0) {
-                preference.setSummary(listPreference.getEntries()[prefIndex]);
+                preference.setSummary(listPref.getEntries()[prefIndex]);
+            }
+        } else if (key.equals(getString(R.string.pref_location_key))) {
+            @SunshineSyncAdapter.LocationStatus int status = Utility.getLocationStatus(this);
+            switch (status) {
+                case SunshineSyncAdapter.LOCATION_STATUS_OK:
+                    preference.setSummary(stringValue);
+                    break;
+                case SunshineSyncAdapter.LOCATION_STATUS_UNKNOWN:
+                    preference.setSummary(getString(
+                            R.string.pref_location_unknown_description, value.toString()));
+                    break;
+                case SunshineSyncAdapter.LOCATION_STATUS_INVALID:
+                    preference.setSummary(getString(
+                            R.string.pref_location_error_description, value.toString()));
+                    break;
+                default:
+                    //if the server is down we still assume the value is valid
+                    preference.setSummary(stringValue);
             }
         } else {
-            // For other preferences, set the summary to the value's simple string representation.
+            //For other preferences, set the summary to the value's simple string representation
             preference.setSummary(stringValue);
         }
     }
+
 
     //this gets called before the preference is changed
     @Override
@@ -98,6 +119,7 @@ public class SettingsActivity extends PreferenceActivity
     // This gets called after the preference is changed, which is important because we
     // start our synchronization here
     @Override
+    @SuppressWarnings("deprecation")
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
         if (key.equals(getString(R.string.pref_location_key))) {
             // we've changed the location
@@ -107,6 +129,10 @@ public class SettingsActivity extends PreferenceActivity
         } else if (key.equals(getString(R.string.pref_units_key))) {
             // units have changed. update lists of weather entries accordingly
             getContentResolver().notifyChange(WeatherContract.WeatherEntry.CONTENT_URI, null);
+        } else if (key.equals(getString(R.string.pref_location_status_key))) {
+            //our location status has changed. Update the summary accordingly
+            Preference locationPreference = findPreference(getString(R.string.pref_location_key));
+            bindPreferenceSummaryToValue(locationPreference);
         }
     }
 
